855d299a50aa95f511fcd8b33f335da608348527
gulzada02 <gulzada1996@gmail.com>
2019-10-24 12:02:00 +0600

    Update script.js

--- Desktop/GitHub/test-task/js/script.js
+++ Desktop/GitHub/test-task/js/script.js
@@ -3,3 +3,12 @@ $('.burger-menu').on('click', function (e) {
     $('.burger-menu').toggleClass('burger-open');
     $('.menu').toggleClass('menu-show');
 });
+
+$(document).ready(function () {
+    $(".projects-slider").owlCarousel({
+        items: 1,
+        dots: true,
+        nav: true,
+        navText: ''
+    });
+});
