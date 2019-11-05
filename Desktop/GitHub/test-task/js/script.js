6155a9f5b547bb3d6efbd55c48d80af0bf746661
gulzada02 <gulzada1996@gmail.com>
2019-11-05 19:05:33 +0600

    add responsive and hover

--- Desktop/GitHub/test-task/js/script.js
+++ Desktop/GitHub/test-task/js/script.js
@@ -6,9 +6,10 @@ $('.burger-menu').on('click', function (e) {
 
 $(document).ready(function () {
     $(".projects-slider").owlCarousel({
-        items: 1,
+        items: 2,
         dots: false,
         nav: true,
+        margin: 30,
         navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #005BC1;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #005BC1;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
         responsive: {
             320: {
@@ -16,6 +17,13 @@ $(document).ready(function () {
             },
             480: {
                 items: 1,
+            },
+
+            992: {
+                items: 2,
+            },
+            1200: {
+                items: 2,
             }
         }
     });
@@ -25,5 +33,20 @@ $(document).ready(function () {
         dots: false,
         nav: true,
         navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #005BC1;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #005BC1;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+        responsive: {
+            320:{
+
+            },
+            480: {
+
+            },
+            992: {
+
+            },
+
+            1200: {
+
+            },
+        }
     });
 });
