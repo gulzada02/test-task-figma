bcc7f555a967f9add38bbb744eade1308fa67e38
gulzada02 <gulzada1996@gmail.com>
2019-11-05 20:16:23 +0600

    changed some responsive mistakes

--- Desktop/GitHub/test-task/js/script.js
+++ Desktop/GitHub/test-task/js/script.js
@@ -33,20 +33,5 @@ $(document).ready(function () {
         dots: false,
         nav: true,
         navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #005BC1;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #005BC1;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
-        responsive: {
-            320:{
-
-            },
-            480: {
-
-            },
-            992: {
-
-            },
-
-            1200: {
-
-            },
-        }
     });
 });
