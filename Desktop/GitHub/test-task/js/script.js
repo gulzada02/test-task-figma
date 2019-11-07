4610d383229f6cfbbd7d624f3bbe87df1baa457e
gulzada02 <gulzada1996@gmail.com>
2019-11-07 13:33:58 +0600

    scroll config

--- Desktop/GitHub/test-task/js/script.js
+++ Desktop/GitHub/test-task/js/script.js
@@ -35,3 +35,12 @@ $(document).ready(function () {
         navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #005BC1;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #005BC1;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
     });
 });
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 80) {
+        $('.scroll-nav').fadeIn('slow');
+        $('#scroll-nav').addClass('header-fixed');
+    } else {
+        $('#scroll-nav').removeClass('header-fixed');
+    }
+});
